0672d35b53d0e00190bb58ab0e76dff4703792f0
funmi7 <talabifunmilayo@gmail.com>
2020-02-14 11:03:30 +0100

    Completed cityreader MVP

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -14,6 +14,18 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+
+import csv
+class City():
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return (f"{self.name}, {self.lat}, {self.lon}")
+
+
 cities = []
 
 def cityreader(cities=[]):
@@ -21,7 +33,13 @@ def cityreader(cities=[]):
   # For each city record, create a new City instance and add it to the 
   # `cities` list
     
-    return cities
+    with open("cities.csv") as cities_file:
+      cities_reader = csv.reader(cities_file, delimiter=',')
+      for row in cities_reader:
+        if row[0] != "city":
+          new_cities = City(row[0], float(row[3]), float(row[4]))
+          cities.append(new_cities)
+      return cities
 
 cityreader(cities)
 
